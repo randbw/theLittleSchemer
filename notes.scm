d31259bc896e1be2bea7647a079a4d4b840d0f2d
cheeseBucket <apbward@gmail.com>
2016-10-31 08:18:38 +1100

    finished up to p79

--- notes.scm
+++ notes.scm
@@ -237,3 +237,69 @@
     (cond
      ((< n m) 0)
      (else (add1 (/ (- n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond
+     ((null? lat) 0)
+     (else
+      (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+     ;;((= 1 n) (car lat))
+     ((zero? (sub1 n)) (car lat))
+     (else
+      (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((zero? (sub1 n)) (cdr lat))
+     (else
+      (cons (car lat)
+	    (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) (quote()))
+     (else
+      (cond
+       ((number? car lat) (no-nums (cdr lat)))
+       (else
+	(cons (car lat) (no-nums (cdr lat)))))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) (quote()))
+     (else
+      (cond
+       ((number? (car lat))
+	(cons (car lat) (all-nums (cdr lat))))
+       (else
+	(all-nums (cdr lat))))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+     ((and (number? a1) (number? a2))
+      (= a1 a2))
+     ((or (number? a1) (number? a2))
+      #f)
+     (else
+      (eq? a1 a2)))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+     ((null? lat) 0)
+     (else
+      (cond
+       ((eqan? a (car lat))
+	(add1 (occur a (cdr lat))))
+       (else
+	(occur a (cdr lat))))))))
+
