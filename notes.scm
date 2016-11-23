773abbfe7816ad8660fdab4bb9ca406d717b74b9
randbw <apbward@gmail.com>
2016-11-24 08:52:40 +1100

    defined union and up to p117

--- notes.scm
+++ notes.scm
@@ -768,4 +768,21 @@
        (member? (car set1) set2)
        (intersect? (cdr set1) set2))))))
 
+(define intersect
+  (lambda (set1 set2)
+    (cond
+     ((null? set1) (quote ()))
+     ((member? (car set1) set2)
+      (cons (car set1)
+	    (intersect (cdr set1) set2)))
+     (else (intersect (cdr set1) set2)))))
 
+(define union
+  (lambda (set1 set2)
+    (cond
+     ((null? set1) set2)
+     ((member? (car set1) set2)
+      (union (cdr set1) set2))
+     (else
+      (cons (car set1)
+	    (union (cdr set1) set2))))))
