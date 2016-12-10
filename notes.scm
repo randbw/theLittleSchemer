27109933b9f70da09df1133da7f9010ed38d23d8
randbw <apbward@gmail.com>
2016-12-10 19:37:28 +1100

    Refactored value using atom-to-function

--- notes.scm
+++ notes.scm
@@ -964,3 +964,21 @@
 
 ;; subst with insert-g
 (define subst (insert-g seq-sub))
+
+(define atom-to-function
+  (lambda (x)
+    (cond
+     ((eq? x (quote +)) +)
+     ((eq? x (quote *)) *)
+     (else ^))))
+
+;; rewrite value using atom-to-function
+(define value
+  (lambda (nexp)
+    (cond
+     ((atom? nexp) nexp)
+     (else
+      ((atom-to-function (operator nexp))
+       (value (1st-sub-exp nexp))
+       (value (2nd-sub-exp nexp)))))))
+
